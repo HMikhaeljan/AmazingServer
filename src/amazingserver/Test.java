5c00a0465086f22a60747bf82ea9c272a55f22ea
HMikhaeljan <Hovsep_@hotmail.com>
2015-12-07 12:37:55 +0100

    Server full set up

---
+++ src/amazingserver/Test.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package amazingserver;
+
+import fontys.observer.BasicPublisher;
+import fontys.observer.RemotePropertyListener;
+import java.rmi.RemoteException;
+import java.rmi.server.UnicastRemoteObject;
+
+/**
+ *
+ * @author Jeroen0606
+ */
+public class Test extends UnicastRemoteObject implements ITest{
+    private BasicPublisher publisher;    
+    
+    public Test() throws RemoteException{
+               
+    }
+    
+    
+    public void addListener(RemotePropertyListener RPL, String property) {
+        publisher.addListener(RPL, property);
+    }    
+
+    @Override
+    public String getText() throws RemoteException {
+        return "HOI KASPÈR!!";
+    }
+}
