656cf9eb7112b540af60afba9bf881e59c9188cc
HMikhaeljan <Hovsep_@hotmail.com>
2015-12-08 13:22:59 +0100

    Fixed Userpackage and added new interface

--- src/UserPackage/UserManager.java
+++ src/UserPackage/UserManager.java
@@ -6,44 +6,65 @@
 package UserPackage;
 
 import Database.DatabaseConnection;
+import Interfaces.ILogin;
+import java.rmi.RemoteException;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.rmi.server.UnicastRemoteObject;
 
 /**
  *
  * @author Hovsep
  */
-public class UserManager {
+public class UserManager extends UnicastRemoteObject implements ILogin {
 
     DatabaseConnection db = new DatabaseConnection();
-    List<User> users;
+    List<User> onlineUsers;
+    List<User> allUsers;
     User newuser;
     Stat stat;
 
-    public void loadallUsers() throws SQLException{
+
+    public UserManager() throws RemoteException{
+    }
+
+    public void loadAllUsers() throws SQLException {
         for (User user : db.getUsers()) {
             newuser = new User(user.getUserID(), user.getName(), user.getPassword(), user.getStats());
+            allUsers.add(newuser);
         }
-        
+
     }
-    
+
+    public List<User> getAllUsers() {
+        return allUsers;
+    }
+
     //vraag alle online users op
+
     public List<User> getOnlineUsers() {
-        return users;
+        return onlineUsers;
     }
 
     /**
      * newUser Creates an account in the database.
      *
-     * @param userid cannot not be null and must be unique.
-     * @param name cannot not be null and must be unique.
+     * @param username
      * @param password cannot not be null.
-     * @param email cannot be null.
      * @param stats cannot be null.
      * @return true if succesfully added in database.
+     * @throws java.sql.SQLException
      */
-    public void newUser(String username, String password, String email) {
-        User user = new User(users.size() + 1, password, email, stat);
+    public void registerUser(String username, String password) {
+        User user = new User(allUsers.size() + 1, username, password, stat);
+        allUsers.add(user);
+        try {
+            db.newUser(username, password);
+        } catch (SQLException ex) {
+            Logger.getLogger(UserManager.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     /**
@@ -51,16 +72,23 @@ public class UserManager {
      * has correspondig account. if User exist then return user with
      * corresponding stats
      *
+     * @param username
      * @param name
      * @param password
      * @return User if account exists otherwise return null.
+     * @throws java.sql.SQLException
      */
-    public User Login(String username, String password) throws SQLException {
+    public User Login(String username, String password) {
 
-        for (User user : db.getUsers()) {
-            if (user.getName().equals(username) && user.getPassword().equals(password)) {
-                return user;
+        try {
+            for (User user : db.getUsers()) {
+                if (user.getName().equals(username) && user.getPassword().equals(password)) {
+                    onlineUsers.add(user);
+                    return user;
+                }
             }
+        } catch (SQLException ex) {
+            Logger.getLogger(UserManager.class.getName()).log(Level.SEVERE, null, ex);
         }
         return null;
     }
